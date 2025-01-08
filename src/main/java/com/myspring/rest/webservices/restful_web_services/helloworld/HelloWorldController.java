1cd40856326377472bf4f6d79c357147c8f6d397
nagarajach <nagarajachowdary@gmail.com>
2025-01-08 17:38:00 -0330

    second commit

---
+++ src/main/java/com/myspring/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.myspring.rest.webservices.restful_web_services.helloworld;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World %s", name));
+	}
+
+}
